055bbc97bd4663d6e1220b7b00f6b86393f5de90
Manolo Salsas <manolez@gmail.com>
2016-05-10 16:01:59 +0200

    QS-989: Styles for blog section

--- src/js/main.js
+++ src/js/main.js
@@ -106,4 +106,30 @@
     }
 
     /** End fixed menu settings (on scroll) **/
+
+    /** Start RSS settings **/
+    var feed = "http://blog.nekuno.com/feed";
+
+    $.ajax(feed, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+
+
+        }
+    });
+    
+    
+    
 })(jQuery);
